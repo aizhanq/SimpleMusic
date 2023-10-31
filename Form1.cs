6b9ffafa11cdfb3e9de5354820c9a1233f0259ea
Aizhan <aizhankarasartova@gmail.com>
2023-10-31 21:42:44 +0600

    drag window by mouse

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,21 @@ namespace MusicPlayerApp
 {
     public partial class MusicPlayerApp : Form
     {
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+        ///
+        /// Handling the window messages
+        ///
+        protected override void WndProc(ref Message message)
+        {
+            base.WndProc(ref message);
+
+            if (message.Msg == WM_NCHITTEST && (int)message.Result == HTCLIENT)
+                message.Result = (IntPtr)HTCAPTION;
+        }
+
         Music model = new Music();
 
         public MusicPlayerApp()
